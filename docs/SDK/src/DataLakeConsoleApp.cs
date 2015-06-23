fb1855bc492f8538325be8a0f8b7594fc40837c2
matt1883 <matt1883@users.noreply.github.com>
2015-06-22 18:14:11 -0700

    Update DataLakeConsoleApp.cs

--- docs/SDK/src/DataLakeConsoleApp.cs
+++ docs/SDK/src/DataLakeConsoleApp.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 using Microsoft.Azure;
 using Microsoft.Azure.Common.Authentication;
+using Microsoft.Azure.Common.Authentication.Models;
 using Microsoft.Azure.Management.DataLake;
 using Microsoft.Azure.Management.DataLake.Models;
 using Microsoft.Azure.Management.DataLakeFileSystem;
 using Microsoft.Azure.Management.DataLakeFileSystem.Models;
 using Microsoft.Azure.Management.DataLakeFileSystem.Uploading;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
 
 namespace DataLakeConsoleApp
 {
@@ -20,49 +22,35 @@ namespace DataLakeConsoleApp
         private DataLakeManagementClient dlClient;
         private DataLakeFileSystemManagementClient dlFileSystemClient;
 
-        private string subscriptionID;
-        private string resourceGroupName;
-        private const string locationStr = "East US 2";
-
-        public TokenCloudCredentials GetCloudCredentials()
+        public DataLakeConsoleApp(Guid subID)
         {
-            string accessToken;
-
-            lock (this)
-            {
-                AuthenticationContext context = new AuthenticationContext("https://login.windows.net/common/");
-
-                AuthenticationResult authResult = context.AcquireToken("https://management.core.windows.net/",
-                                                                       "1950a258-227b-4e31-a9cf-717495945fc2",
-                                                                       new Uri("urn:ietf:wg:oauth:2.0:oob"));
-                if (authResult == null)
-                    throw new InvalidOperationException("Failed to obtain the access token");
-
-                accessToken = authResult.AccessToken;
-            }
-
-            return new TokenCloudCredentials(subscriptionID, accessToken);
+            InitializeClients(subID);
         }
 
-        public DataLakeConsoleApp(string subID, string rgName)
+        public void InitializeClients(Guid subID)
         {
-            this.subscriptionID = subID;
-            this.resourceGroupName = rgName;
+            ProfileClient profileClient = new ProfileClient(new AzureProfile());
+            AzureEnvironment env = profileClient.GetEnvironmentOrDefault(EnvironmentName.AzureCloud);
+            AzureAccount acct = new AzureAccount { Type = AzureAccount.AccountType.User };
 
-            TokenCloudCredentials cc = GetCloudCredentials();
+            profileClient.InitializeProfile(env, subID, acct, null, "");
 
-            this.dlClient = new DataLakeManagementClient(cc);
-            this.dlFileSystemClient = new DataLakeFileSystemManagementClient(cc);
+            AzureSubscription subscription = profileClient.Profile.Subscriptions.Values.FirstOrDefault(s => s.Id.Equals(subID));
+
+            profileClient.SetSubscriptionAsDefault(subscription.Id, subscription.Account);
+
+            SubscriptionCloudCredentials credentials = AzureSession.AuthenticationFactory.GetSubscriptionCloudCredentials(profileClient.Profile.Context);
+
+            // Instantiate clients
+            this.dlClient = new DataLakeManagementClient(credentials, profileClient.Profile.Context.Environment.GetEndpointAsUri(AzureEnvironment.Endpoint.ResourceManager));
+            this.dlFileSystemClient = new DataLakeFileSystemManagementClient(credentials, profileClient.Profile.Context.Environment.GetEndpoint(AzureEnvironment.Endpoint.AzureDataLakeFileSystemEndpointSuffix));
         }
 
         public bool UploadFile(string dlAccountName, string srcPath, string destPath)
         {
             UploadParameters parameters = new UploadParameters(srcPath, destPath, dlAccountName);
-
             DataLakeFrontEndAdapter frontend = new DataLakeFrontEndAdapter(dlAccountName, dlFileSystemClient);
-
             DataLakeUploader uploader = new DataLakeUploader(parameters, frontend);
-
             uploader.Execute();
 
             return true;
@@ -72,15 +60,14 @@ namespace DataLakeConsoleApp
         {
             var response = dlFileSystemClient.FileSystem.BeginAppend(path, dlAccountName, null);
             dlFileSystemClient.FileSystem.Append(response.Location, streamContents);
-
             return true;
         }
 
         static void Main(string[] args)
         {
-            DataLakeConsoleApp app = new DataLakeConsoleApp("00000000-1111-2222-3333-444444444444", "myresourcegroup");
+            DataLakeConsoleApp app = new DataLakeConsoleApp(new Guid("12345678-1234-1234-1234-123456789012"));
 
-            app.UploadFile("mydatalake", @"C:\foo.txt", @"foo_uploaded.txt");
+            app.UploadFile("mydatalakeaccount", @"C:\foo.txt", @"foo_uploaded.txt");
 
             System.Console.ReadKey();
         }
