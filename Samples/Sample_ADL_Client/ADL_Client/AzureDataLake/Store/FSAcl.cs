14c11018760df9f6d69c5558014227e34c32c7ea
Saveen Reddy <saveenr@live.com>
2016-10-02 14:29:09 -0700

    Added FsEntry enumeration for acl

--- Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/Store/FSAcl.cs
+++ Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/Store/FSAcl.cs
@@ -1,5 +1,15 @@
+using System.Collections.Generic;
+
 namespace AzureDataLake.Store
 {
+    public class FsAclEntry
+    {
+        public string Type;
+        public string MiddleThing;
+        public FsPermission? Permission;
+    }
+
+
     public class FsAcl
     {
         public string Group;
@@ -8,6 +18,7 @@ namespace AzureDataLake.Store
         public FsPermission? GroupPermission;
         public FsPermission? OtherPermission;
 
+        public List<FsAclEntry> Entries;
 
         public FsAcl(Microsoft.Azure.Management.DataLake.Store.Models.AclStatus acl)
         {
@@ -29,6 +40,18 @@ namespace AzureDataLake.Store
                 this.OwnerPermission = new AzureDataLake.Store.FsPermission(int.Parse(s[0].ToString()));
                 this.GroupPermission = new AzureDataLake.Store.FsPermission(int.Parse(s[1].ToString()));
                 this.OtherPermission = new AzureDataLake.Store.FsPermission(int.Parse(s[2].ToString()));
+            }
+
+            this.Entries = new List<FsAclEntry>( acl.Entries.Count);
+            foreach (string e in acl.Entries)
+            {
+                var tokens = e.Split(':');
+                var e2 = new FsAclEntry();
+                e2.Type = tokens[0];
+                e2.MiddleThing= tokens[1];
+                e2.Permission= new FsPermission(tokens[2]);
+
+                this.Entries.Add(e2);
             }
         }
     }
