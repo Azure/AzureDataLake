afa89aaf657ddcc21a9520a3852823975b1a1c92
Saveen Reddy <saveenr@live.com>
2016-10-02 19:15:43 -0700

    Split unit tests apart

--- Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Filesystem_Tests.cs
+++ Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Filesystem_Tests.cs
@@ -6,28 +6,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ADL_Client_Tests
 {
     [TestClass]
-    public class Store_Tests : Base_Tests
+    public class Store_Filesystem_Tests : Base_Tests
     {
- 
-
-        [TestMethod]
-        public void Get_AD_Tenant_ID()
-        {
-            this.Initialize();
-            var directory = AzureDataLake.Authentication.Directory.Resolve("microsoft.com");
-        }
-
-        [TestMethod]
-        public void List_ADLS_Accounts()
-        {
-            this.Initialize();
-            var adls_accounts = this.adls_mgmt_client.ListAccounts();
-            foreach (var a in adls_accounts)
-            {
-                System.Console.WriteLine("Store {0} ", a.Name);
-            }
-        }
-
         [TestMethod]
         public void List_Files_Recursive()
         {
---
+++ Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Management_Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADL_Client_Tests
+{
+    [TestClass]
+    public class Store_Management_Tests : Base_Tests
+    {
+ 
+
+        [TestMethod]
+        public void Get_AD_Tenant_ID()
+        {
+            this.Initialize();
+            var directory = AzureDataLake.Authentication.Directory.Resolve("microsoft.com");
+        }
+
+        [TestMethod]
+        public void List_ADLS_Accounts()
+        {
+            this.Initialize();
+            var adls_accounts = this.adls_mgmt_client.ListAccounts();
+            foreach (var a in adls_accounts)
+            {
+                System.Console.WriteLine("Store {0} ", a.Name);
+            }
+        }
+
+    }
+}
+
