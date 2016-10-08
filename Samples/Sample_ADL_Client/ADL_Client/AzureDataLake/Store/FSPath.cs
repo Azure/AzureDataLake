2ff2607df2efd1237758030d7ebd3a759b301c51
Saveen Reddy <saveenr@live.com>
2016-10-08 15:16:18 -0700

    Added an FsUri class

--- Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/Store/FSPath.cs
+++ Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/Store/FSPath.cs
@@ -1,5 +1,96 @@
 namespace AzureDataLake.Store
 {
+
+    public class FsUri
+    {
+        public readonly string Account;
+        public readonly string Path;
+
+        public FsUri(string uri):
+            this( new System.Uri(uri))
+        {                       
+        }
+
+        public FsUri(string account, string path)
+        {
+            if (account == null)
+            {
+                throw new System.ArgumentNullException(nameof(account));
+            }
+
+            if (account.Length==0)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(account));
+            }
+
+            this.Account = account.ToLower();
+
+
+            if (path == null || path == "" || path == "/" || path == "\\" )
+            {
+                this.Path = "/";
+            }
+            else
+            {
+                if (path.StartsWith("/") || path.StartsWith("\\"))
+                {
+                    this.Path = path;
+                }
+                else
+                {
+                    this.Path = "/" + path;
+                }
+            }
+
+        }
+        public FsUri(System.Uri uri)
+        {
+            // adl://usqlteamdemosadls.azuredatalakestore.net/users/mwinkle
+            // swebhdfs://usqlteamdemosadls.azuredatalakestore.net/users/mwinkle
+            string scheme = uri.Scheme;
+            scheme = scheme.ToLowerInvariant();
+
+            // validate the URI scheme
+
+            if (!(scheme == "adl" || scheme == "swebhdfs"))
+            {
+                throw new System.ArgumentException("Unsupported URI scheme");
+            }
+
+            // Figure out account name from URI host
+
+            string lowerhost = uri.Host.ToLowerInvariant();
+            var tokens = lowerhost.Split('.');
+            if (tokens.Length == 1)
+            {
+                string account = tokens[0];
+                this.Account = account;
+            }
+            else if (tokens.Length == 3)
+            {
+                if (tokens[1] != "azuredatalakestore")
+                {
+                    throw new System.ArgumentException("Invalid hostname");
+                }
+
+                if (tokens[2] != "net")
+                {
+                    throw new System.ArgumentException("Invalid hostname");
+                }
+
+                string account = tokens[0];
+                this.Account = account;
+            }
+            else
+            {
+                throw new System.ArgumentException("Invalid hostname");
+            }
+
+            string localpath = uri.LocalPath;
+            this.Path = localpath;
+        }
+
+    }
     public class FsPath
     {
         string s;
---
+++ Samples/Sample_ADL_Client/ADL_Client_Tests/FSUri_Tests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADL_Client_Tests
+{
+    [TestClass]
+    public class FSUri_Tests : Base_Tests
+    {
+        [TestMethod]
+        public void FSPath_Constructor_Root_1()
+        {
+            var u0 = new AzureDataLake.Store.FsUri("adl://ACCOUNT.azuredatalakestore.net/users/USER1");
+            Assert.AreEqual("account",u0.Account);
+            Assert.AreEqual("/users/USER1", u0.Path);
+        }
+
+        [TestMethod]
+        public void FSPath_Constructor_Root_2()
+        {
+            var u0 = new AzureDataLake.Store.FsUri("adl://ACCOUNT.azuredatalakestore.net/users/");
+            Assert.AreEqual("account", u0.Account);
+            Assert.AreEqual("/users/", u0.Path);
+        }
+
+        [TestMethod]
+        public void FSPath_Constructor_Root_3()
+        {
+            var u0 = new AzureDataLake.Store.FsUri("adl://ACCOUNT.azuredatalakestore.net/users");
+            Assert.AreEqual("account", u0.Account);
+            Assert.AreEqual("/users", u0.Path);
+        }
+
+        [TestMethod]
+        public void FSPath_Constructor_Root_4()
+        {
+            var u0 = new AzureDataLake.Store.FsUri("adl://ACCOUNT.azuredatalakestore.net/");
+            Assert.AreEqual("account", u0.Account);
+            Assert.AreEqual("/", u0.Path);
+        }
+
+        [TestMethod]
+        public void FSPath_Constructor_Root_5()
+        {
+            var u0 = new AzureDataLake.Store.FsUri("adl://ACCOUNT.azuredatalakestore.net");
+            Assert.AreEqual("account", u0.Account);
+            Assert.AreEqual("/", u0.Path);
+        }
+
+        [TestMethod]
+        public void FSPath_Constructor_Root_6()
+        {
+            var u0 = new AzureDataLake.Store.FsUri("ACCOUNT",null);
+            Assert.AreEqual("account", u0.Account);
+            Assert.AreEqual("/", u0.Path);
+
+            var u1 = new AzureDataLake.Store.FsUri("ACCOUNT", "");
+            Assert.AreEqual("account", u1.Account);
+            Assert.AreEqual("/", u1.Path);
+
+            var u2 = new AzureDataLake.Store.FsUri("ACCOUNT", "\\");
+            Assert.AreEqual("account", u2.Account);
+            Assert.AreEqual("/", u2.Path);
+
+            var u3 = new AzureDataLake.Store.FsUri("ACCOUNT", "/");
+            Assert.AreEqual("account", u3.Account);
+            Assert.AreEqual("/", u3.Path);
+
+        }
+
+
+    }
+}
