4f60b2a664ba664a0b953020402f82316aefd9ce
Saveen Reddy <saveenr@live.com>
2016-10-09 12:30:32 -0700

    Added test for file expiry absolute

--- Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Filesystem_Expiry_Tests.cs
+++ Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Filesystem_Expiry_Tests.cs
@@ -26,7 +26,7 @@ namespace ADL_Client_Tests
             var before_fstat1 = this.adls_fs_client.GetFileStatus(fname1);
 
             // Having no expiry is the same as having an expiry of the UNix Epoch (1970/1/1)
-            var before_exp = FsUnixTime.ToToDateTimeUtc(before_fstat1.ExpirationTime.Value.MillisecondsSinceEpoch);
+            var before_exp = before_fstat1.ExpirationTime.Value.ToToDateTimeUtc();
             Assert.AreEqual(FsUnixTime.EpochDateTime, before_exp);
         }
 
@@ -47,10 +47,38 @@ namespace ADL_Client_Tests
             this.adls_fs_client.SetFileExpiryRelativeToNow(fname1, 60 * 60 * 24 * 1000);
 
             var end_fstat1 = this.adls_fs_client.GetFileStatus(fname1);
-            var end_exp = FsUnixTime.ToToDateTimeUtc(end_fstat1.ExpirationTime.Value.MillisecondsSinceEpoch);
+            var end_exp = end_fstat1.ExpirationTime.Value.ToToDateTimeUtc();
 
             var dif = end_exp - now;
             Assert.AreEqual(1.0, dif.TotalDays, 0.0001);
+
+            this.adls_fs_client.Delete(dir, true);
+            Assert.IsFalse(this.adls_fs_client.Exists(fname1));
+            Assert.IsFalse(this.adls_fs_client.Exists(dir));
+        }
+
+        [TestMethod]
+        public void File_Expiry_Absolute()
+        {
+            this.Initialize();
+            var dir = create_test_dir();
+
+            var fname1 = dir.Append("foo.txt");
+
+            var cfo = new AzureDataLake.Store.CreateFileOptions();
+            cfo.Overwrite = true;
+
+            this.adls_fs_client.CreateFileWithContent(fname1, "Hello", cfo);
+
+            var now = System.DateTime.UtcNow;
+            var future = now.AddDays(365);
+            this.adls_fs_client.SetFileExpiryAbsolute(fname1, future);
+
+            var end_fstat1 = this.adls_fs_client.GetFileStatus(fname1);
+            var end_exp = end_fstat1.ExpirationTime.Value.ToToDateTimeUtc();
+
+            var dif = end_exp - now;
+            Assert.AreEqual(365.0, dif.TotalDays, 0.0001);
 
             this.adls_fs_client.Delete(dir, true);
             Assert.IsFalse(this.adls_fs_client.Exists(fname1));
--- Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Filesystem_Tests.cs
+++ Samples/Sample_ADL_Client/ADL_Client_Tests/Store_Filesystem_Tests.cs
@@ -111,42 +111,6 @@ namespace ADL_Client_Tests
 
         }
 
-        [TestMethod]
-        public void Basic_File_Expiry_Scenario()
-        {
-            this.Initialize();
-            var dir = create_test_dir();
-
-            var fname1 = dir.Append("foo.txt");
-
-            var cfo = new AzureDataLake.Store.CreateFileOptions();
-            cfo.Overwrite = true;
-
-            this.adls_fs_client.CreateFileWithContent(fname1, "Hello", cfo);
-
-            var before_fstat1 = this.adls_fs_client.GetFileStatus(fname1);
-
-            // Having no expiry is the same as having an expiry of the UNix Epoch (1970/1/1)
-            var before_exp = FsUnixTime.ToToDateTimeUtc(before_fstat1.ExpirationTime.Value.MillisecondsSinceEpoch);
-            Assert.AreEqual(FsUnixTime.EpochDateTime,before_exp);
-
-            var now = System.DateTime.UtcNow;
-            this.adls_fs_client.SetFileExpiryRelativeToNow(fname1, 60 * 60 * 24 * 1000);
-
-            var end_fstat1 = this.adls_fs_client.GetFileStatus(fname1);
-            var end_exp = FsUnixTime.ToToDateTimeUtc(end_fstat1.ExpirationTime.Value.MillisecondsSinceEpoch);
-
-            var dif = end_exp - now;
-            Assert.AreEqual(1.0,dif.TotalDays,0.0001);
-
-            int x = 1;
-
-            this.adls_fs_client.Delete(dir, true);
-            Assert.IsFalse(this.adls_fs_client.Exists(fname1));
-            Assert.IsFalse(this.adls_fs_client.Exists(dir));
-
-        }
-
         private FsPath create_test_dir()
         {
             var dir = new AzureDataLake.Store.FsPath("/test_adl_demo_client");
