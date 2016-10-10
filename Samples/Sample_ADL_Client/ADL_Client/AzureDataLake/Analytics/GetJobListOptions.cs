9a581a7cf393e836c5c2b752b3c9377a9420a404
Saveen Reddy <saveenr@live.com>
2016-10-10 15:51:53 -0700

    Added strogner OData query support

--- Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/Analytics/GetJobListOptions.cs
+++ Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/Analytics/GetJobListOptions.cs
@@ -1,6 +1,114 @@
 using System.Collections.Generic;
 using System.Linq;
 
+namespace AzureDataLake.ODataQuery
+{
+    public abstract class Expr
+    {
+
+        public abstract string ToExprString();
+    }
+
+    public class ExprAnd : Expr
+    {
+        public List<Expr> Items;
+        public ExprAnd(params Expr[] items)
+        {
+            this.Items = new List<Expr>();
+            this.Items.AddRange(items);
+        }
+
+        public override string ToExprString()
+        {
+            return string.Join(" and ", this.Items.Select( s=> "("+s.ToExprString()+")"));
+        }
+    }
+
+    public class ExprOr : Expr
+    {
+        public List<Expr> Items;
+        public ExprOr(params Expr[] items)
+        {
+            this.Items = new List<Expr>();
+            this.Items.AddRange(items);
+        }
+
+        public override string ToExprString()
+        {
+            return string.Join(" or ", this.Items.Select(s => "(" + s.ToExprString() + ")"));
+        }
+    }
+
+    public class ExprParens : Expr
+    {
+        public Expr Item;
+        public ExprParens( Expr item)
+        {
+            this.Item = item;
+        }
+
+        public override string ToExprString()
+        {
+            return string.Format("({0})",this.Item.ToExprString());
+        }
+    }
+
+    public class ExprStringEquals : Expr
+    {
+        public string Column;
+        public string Value;
+        public ExprStringEquals(string col, string val)
+        {
+            this.Column = col;
+            this.Value = val;
+        }
+
+        public override string ToExprString()
+        {
+            return string.Format("{0} eq '{1}'", this.Column, this.Value);
+        }
+    }
+
+    public class ExprDateTimeAfter : Expr
+    {
+        public string Column;
+        public System.DateTime Value;
+        public ExprDateTimeAfter(string col, System.DateTime val)
+        {
+            this.Column = col;
+            this.Value = val;
+        }
+
+        public override string ToExprString()
+        {
+            string datestring = this.Value.ToString("O");
+
+            // due to issue: https://github.com/Azure/autorest/issues/975,
+            // date time offsets must be explicitly escaped before being passed to the filter
+
+            var escaped_datestring = System.Uri.EscapeDataString(datestring);
+
+            var expr = string.Format("{0} ge datetimeoffset'{1}'", this.Column, escaped_datestring);
+            return expr;
+        }
+    }
+
+    public class ExprRaw : Expr
+    {
+        public string Item;
+        public ExprRaw(string s)
+        {
+            this.Item = s;
+        }
+
+        public override string ToExprString()
+        {
+            return this.Item;
+        }
+    }
+
+}
+
 namespace AzureDataLake.Analytics
 {
     public class GetJobListOptions
@@ -43,77 +151,47 @@ namespace AzureDataLake.Analytics
 
         public string CreateFilterString()
         {
-            var filter = new List<string>();
+
+            var q = new AzureDataLake.ODataQuery.ExprAnd();
+
             if (!string.IsNullOrEmpty(this.FilterSubmitter))
             {
-                string expr = ExprStringEquals("submitter",this.FilterSubmitter);
-                filter.Add(expr);
+                q.Items.Add( new AzureDataLake.ODataQuery.ExprStringEquals("submitter",this.FilterSubmitter));
             }
 
             if (this.FilterSubmittedAfter.HasValue)
             {
-                string expr = ExprDateTimeGreaterThan("name", this.FilterSubmittedAfter.Value);
-                filter.Add(expr);
+                q.Items.Add(new AzureDataLake.ODataQuery.ExprDateTimeAfter("submittedafter", this.FilterSubmittedAfter.Value));
             }
 
             if (!string.IsNullOrEmpty(this.FilterName))
             {
-                string expr = string.Format("name eq '{0}'", this.FilterName);
-                filter.Add(expr);
+                q.Items.Add(new AzureDataLake.ODataQuery.ExprStringEquals("name", this.FilterName));
             }
             
             if (this.FilterState != null && this.FilterState.Length > 0)
             {
-                var states = this.FilterState.Select(state => state.ToString());
-                states = states.Select(s=>ExprStringEquals("state",s));
-                string expr = ExprParens( ExprOr(states) );
-                filter.Add(expr);
+                var expr_and = new AzureDataLake.ODataQuery.ExprOr();
+                q.Items.Add(expr_and);
+                foreach (var state in this.FilterState)
+                {
+                    expr_and.Items.Add( new AzureDataLake.ODataQuery.ExprStringEquals("state", state.ToString()));
+                }
             }
             
             if (this.FilterResult != null && this.FilterResult.Length > 0)
             {
-                var results = this.FilterResult.Select(s => s.ToString());
-                results = results.Select(s => ExprStringEquals("result", s));
-                var expr = ExprParens( ExprOr(results) );
-                filter.Add(expr);
+                var expr_and = new AzureDataLake.ODataQuery.ExprOr();
+                q.Items.Add(expr_and);
+                foreach (var result in this.FilterResult)
+                {
+                    expr_and.Items.Add(new AzureDataLake.ODataQuery.ExprStringEquals("result", result.ToString()));
+                }
+
             }
 
-            var filterString = ExprAnd(filter);
+            var filterString = q.ToExprString();
             return filterString;
-        }
-
-        public string ExprStringEquals(string prop, string value)
-        {
-            return string.Format("{0} eq '{1}'", prop, value);
-        }
-
-        public string ExprParens(string s)
-        {
-            return "(" + s + ")";
-        }
-
-        public string ExprAnd(IEnumerable<string> items)
-        {
-            return string.Join(" and ", items);
-        }
-
-        public string ExprOr(IEnumerable<string> items)
-        {
-            return string.Join(" or ", items);
-        }
-
-        public string ExprDateTimeGreaterThan(string name, System.DateTime value)
-        {
-
-            string datestring = value.ToString("O");
-
-            // due to issue: https://github.com/Azure/autorest/issues/975,
-            // date time offsets must be explicitly escaped before being passed to the filter
-
-            var escaped_datestring = System.Uri.EscapeDataString(datestring);
-
-            var expr = string.Format("{0} ge datetimeoffset'{1}'", name, escaped_datestring);
-            return expr;
         }
     }
 }
