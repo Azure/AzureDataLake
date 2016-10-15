2621ee92d29c299f9b1e530d7e3944adc5ccfb67
saveenr <saveenr@SAVEENRX2>
2016-10-15 02:01:24 -0700

    impoved integer filter

--- Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/OData/Utils/PropertyFilterInteger.cs
+++ Samples/Sample_ADL_Client/ADL_Client/AzureDataLake/OData/Utils/PropertyFilterInteger.cs
@@ -4,47 +4,65 @@ namespace AzureDataLake.ODataQuery
 {
     public class PropertyFilterInteger : PropertyFilter
     {
-        private int? before_value;
-        private int? after_value;
+        private int? upper_value;
+        private int? lower_value;
         private int? exactly_value;
+        public bool IsInclusive;
 
         public PropertyFilterInteger(string field_name) :
             base(field_name)
         {
         }
+
         public void Before(int value)
         {
-            this.before_value = value;
+            this.upper_value = value;
+            this.lower_value = null;
+            this.exactly_value = null;
         }
 
         public void After(int value)
         {
-            this.after_value = value;
+            this.upper_value = null;
+            this.lower_value = value;
+            this.exactly_value = null;
         }
+
+        public void InRange(int lower, int upper)
+        {
+            this.upper_value = upper;
+            this.lower_value = lower;
+            this.exactly_value = null;
+        }
+
 
         public void Exactly(int value)
         {
+            this.upper_value = null;
+            this.lower_value = null;
             this.exactly_value = value;
         }
         
         public override ODataQuery.Expr ToExpression()
         {
-            if (!(this.before_value.HasValue || this.after_value.HasValue || this.exactly_value.HasValue))
+            if (!(this.upper_value.HasValue || this.lower_value.HasValue || this.exactly_value.HasValue))
             {
                 return null;
             }
 
             var expr_and = new ODataQuery.ExprLogicalAnd();
 
-            if (this.before_value.HasValue)
+            if (this.upper_value.HasValue)
             {
-                var expr_compare = new ODataQuery.ExprCompareNumeric(this.expr_field, new ODataQuery.ExprLiteralInt(this.before_value.Value), ComparisonNumeric.LesserThan);
+                var op = this.IsInclusive ? ComparisonNumeric.LesserThanOrEquals : ComparisonNumeric.LesserThan;
+                var expr_compare = new ODataQuery.ExprCompareNumeric(this.expr_field, new ODataQuery.ExprLiteralInt(this.upper_value.Value), op );
                 expr_and.Add(expr_compare);
             }
 
-            if (this.after_value.HasValue)
+            if (this.lower_value.HasValue)
             {
-                var expr_compare = new ODataQuery.ExprCompareNumeric(this.expr_field, new ODataQuery.ExprLiteralInt(this.after_value.Value), ComparisonNumeric.GreaterThan);
+                var op = this.IsInclusive ? ComparisonNumeric.GreaterThanOrEquals : ComparisonNumeric.GreaterThan;
+                var expr_compare = new ODataQuery.ExprCompareNumeric(this.expr_field, new ODataQuery.ExprLiteralInt(this.lower_value.Value), op );
                 expr_and.Add(expr_compare);
             }
 
