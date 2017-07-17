9e3607492c549a1ba478a28418c044a01ebf4ca2
Matthew Hicks <matt1883@users.noreply.github.com>
2017-07-17 13:01:10 -0700

    Update Set-AdlsAccess.ps1
    Adding options to change file/folder owner recursively

--- Samples/PowerShell/ADLSUser/Set-AdlsAccess.ps1
+++ Samples/PowerShell/ADLSUser/Set-AdlsAccess.ps1
@@ -3,20 +3,26 @@
 Recursively sets permissions on a Data Lake Store filesystem, at the specified path.
 
 .DESCRIPTION
-Recursively sets permissions on a Data Lake Store filesystem, 
-at the specified path, for either (1) a given "User" or service 
+For -SetAclEntry, this script recursively adds/modifies an Access Control List (ACL) entry at the specified path for (1) a given "User" or service 
 principal, (2) a given "Group", or (3) "Other".
+For -SetOwner, this script recursively sets the user or group owner at the specified path for (1) a given "User" or service principal or (2) a given "Group".
 
 .PARAMETER Account
-The name of the Data Lake Store account to which to add or remove the user, group or other permission.
+The name of the Data Lake Store account.
 
-.PARAMETER EntityIdToAdd
-The Azure AD Object ID of the user or group to add. This should not be specified for 'other' permissions.
-The recommendation to ensure the right user or group is added is to run Get-AzureRMAdUser or 
-Get-AzureRMAdGroup and pass in the Object ID returned by that cmdlet.
+.PARAMETER EntityId
+The Azure AD Object ID of the specified user or group. This should not be specified for 'other' permissions.
+The recommendation to ensure the right user or group is added is to run Get-AzureRmAdUser or 
+Get-AzureRmAdGroup and pass in the Object ID returned by that cmdlet.
 
 .PARAMETER EntityType
-Indicates if the entity to be added is a user, group or the other permission.
+Indicates if the entity to be added is a User or Group, or if the Other permission is being set.
+
+.PARAMETER SetOwner
+Indicates that the user or group owner of the affected files or folders should be modified.
+
+.PARAMETER SetAclEntry
+Indicates that ACL entries should be added or modified on the affected files or folders.
 
 .PARAMETER Path
 The path to start giving the specified entity permissions. This will also recursively propagate those permissions.
@@ -26,21 +32,35 @@ The permissions to give the user, group or other. This can be "All", "ReadExecut
 
 .EXAMPLE
 $objectId = (Get-AzureRmAdUser -Mail john@contoso.com).Id
-Set-AdlsAccess.ps1 -Account myadlsaccount -EntityIdToAdd $objectId -EntityType User
+Set-AdlsAccess.ps1 -Account contosoadlsaccount -Path / -SetAclEntry -EntityId $objectId -EntityType User -Permissions All
+
+.EXAMPLE
+$objectId = (Get-AzureRmAdGroup -Mail securitygroup@contoso.com).Id
+Set-AdlsAccess.ps1 -Account -Path / contosoadlsaccount -SetOwner -EntityId $objectId -EntityType User
 #>
 param
 (
     [Parameter(Mandatory=$true)]
     [string] $Account,
+
+    [Parameter(Mandatory=$true)]
+    [string] $Path,
+
+    [Parameter(ParameterSetName='SetAclEntry')]
+    [switch] $SetAclEntry,
+
+    [Parameter(ParameterSetName='SetOwner')]
+    [switch] $SetOwner,
+
     [Parameter(Mandatory=$false)]
-    [string] $EntityIdToAdd,
+    [string] $EntityId,
+
     [ValidateSet("User", "Group", "Other")]
     [Parameter(Mandatory=$true)]
     [string] $EntityType,
-    [Parameter(Mandatory=$true)]
-    [string] $Path,
+
     [ValidateSet("All", "ReadExecute", "None")]
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true, ParameterSetName='SetAclEntry')]
     [string] $Permissions
 )
 
@@ -53,30 +73,40 @@ function giveaccess
         [Parameter(Mandatory=$true)]
         [string] $Path,
         [Parameter(Mandatory=$false)]
-        [string] $IdToAdd,
+        [string] $Id,
         [Parameter(Mandatory=$true)]
-        [string] $entityType,
+        [string] $EntityType,
         [Parameter(Mandatory=$true)]
-        [string] $permissionToAdd,
+        [string] $Permissions,
         [Parameter(Mandatory=$false)]
-        [switch] $isDefault = $false,
-        [Parameter(Mandatory=$true)]
-        [string] $loginProfilePath
+        [switch] $IsDefault = $false
     )
     
     # There is not an easy way to check if the user is part of an existing security group with permissions, so we are going to need to just add the ACE
 
-    $aceToAdd = "$entityType`:$idToAdd`:$permissionToAdd"
+    if ($Permissions -ieq "All")
+    {
+        $perms = "rwx";
+    }
+    elseif ($Permissions -ieq "ReadExecute")
+    {
+        $perms = "r-x";
+    }
+    else
+    {
+        $perms = "---";
+    }
+
+    $aceToAdd = "$entityType`:$Id`:$perms"
     if($isDefault)
     {
         $aceToAdd = @("default:$aceToAdd","$aceToAdd")
     }
     
-    Select-AzureRMProfile -Path $loginProfilePath | Out-Null
-    Set-AzureRmDataLakeStoreItemAclEntry -Account $Account -Path $Path -Acl $aceToAdd
+    Set-AzureRmDataLakeStoreItemAclEntry -Account $Account -Path $Path -Acl $aceToAdd | Out-Null
 }
 
-function copyacls
+function setacerec
 {
     param
     (
@@ -87,11 +117,9 @@ function copyacls
         [Parameter(Mandatory=$true)]
         [string] $Permissions,
         [Parameter(Mandatory=$false)]
-        [string] $IdToAdd,
+        [string] $Id,
         [Parameter(Mandatory=$true)]
-        [string] $entityType,
-        [Parameter(Mandatory=$true)]
-        [string] $loginProfilePath
+        [string] $EntityType
     )
     
     $itemList = Get-AzureRMDataLakeStoreChildItem -Account $Account -Path $Path;
@@ -100,29 +128,52 @@ function copyacls
         $pathToSet = Join-Path -Path $Path -ChildPath $item.PathSuffix;
         $pathToSet = $pathToSet.Replace("\", "/");
         
-        if ($Permissions -ieq "All")
-        {
-            $perms = "rwx";
-        }
-        elseif ($Permissions -ieq "ReadExecute")
-        {
-            $perms = "r-x";
-        }
-        else
-        {
-            $perms = "---";
-        }
-        
         if ($item.Type -ieq "FILE")
         {
             # set the ACL without default
-            giveaccess -Account $Account -Path $pathToSet -IdToAdd $IdToAdd -entityType $entityType -permissionToAdd $perms -loginProfilePath $loginProfilePath | Out-Null
+            giveaccess -Account $Account -Path $pathToSet -Id $Id -EntityType $EntityType -Permissions $Permissions | Out-Null
         }
         elseif ($item.Type -ieq "DIRECTORY")
         {
             # set permission and recurse on the directory
-            giveaccess -Account $Account -Path $pathToSet -IdToAdd $IdToAdd -entityType $entityType -permissionToAdd $perms -isDefault -loginProfilePath $loginProfilePath | Out-Null
-            copyacls -Account $Account -Path $pathToSet -Permissions $Permissions -IdToAdd $IdToAdd -entityType $entityType -loginProfilePath $loginProfilePath | Out-Null
+            giveaccess -Account $Account -Path $pathToSet -Id $Id -EntityType $EntityType -Permissions $Permissions -IsDefault | Out-Null
+            setacerec -Account $Account -Path $pathToSet -Permissions $Permissions -Id $Id -EntityType $EntityType | Out-Null
+        }
+        else
+        {
+            throw "Invalid path type of: $($item.Type). Valid types are 'DIRECTORY' and 'FILE'"
+        }
+    }
+}
+
+function setownerrec
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $Account,
+        [Parameter(Mandatory=$true)]
+        [string] $Path,
+        [Parameter(Mandatory=$false)]
+        [string] $Id,
+        [Parameter(Mandatory=$true)]
+        [string] $EntityType
+    )
+
+    $itemList = Get-AzureRMDataLakeStoreChildItem -Account $Account -Path $Path;
+    foreach($item in $itemList)
+    {
+        $pathToSet = Join-Path -Path $Path -ChildPath $item.PathSuffix;
+        $pathToSet = $pathToSet.Replace("\", "/");
+        
+        if ($item.Type -ieq "FILE")
+        {
+            Set-AzureRmDataLakeStoreItemOwner -Account $Account -Path $Path -Type $EntityType -Id $EntityId | Out-Null
+        }
+        elseif ($item.Type -ieq "DIRECTORY")
+        {
+            Set-AzureRmDataLakeStoreItemOwner -Account $Account -Path $Path -Type $EntityType -Id $EntityId | Out-Null
+            setownerrec -Account $Account -Path $pathToSet -Id $Id -EntityType $EntityType | Out-Null
         }
         else
         {
@@ -134,49 +185,81 @@ function copyacls
 # This script assumes the following:
 # 1. The Azure PowerShell environment is installed
 # 2. The current session has already run "Login-AzureRMAccount" with a user account that has permissions to the specified ADLS account
-try
-{   if($EntityType -ieq "other" -and !([string]::IsNullOrEmpty($EntityIdToAdd)))
-	{
-		throw "EntityIdToAdd is not supported when modifying permissions for 'Other' entity types"
-	}
+
+switch ($PsCmdlet.ParameterSetName) 
+{ 
+    “SetOwner”  {
+        try
+        {
+            if($EntityType -ieq "other")
+	        {
+		        throw "SetOwner is not supported when 'Other' EntityType is specified"
+	        }
 	
-	if($EntityType -ine "other" -and [string]::IsNullOrEmpty($EntityIdToAdd))
-	{
-		throw "EntityIdToAdd is required when modifying permissions for 'user' and 'group' entity types"
-	}
+	        if($EntityType -ine "other" -and [string]::IsNullOrEmpty($EntityId))
+	        {
+		        throw "EntityId is required when SetOwner is specified"
+	        }
 	
-	$ignored = [Guid]::Empty
-	if($EntityType -ine "other" -and !([Guid]::TryParse($EntityIdToAdd, [ref]$ignored)))
-	{
-		throw "EntityIdToAdd must be a valid GUID. EntityIdToAdd value was: $EntityIdToAdd"
-	}
+	        $ignored = [Guid]::Empty
+	        if($EntityType -ine "other" -and !([Guid]::TryParse($EntityId, [ref]$ignored)))
+	        {
+		        throw "EntityId must be a valid GUID. EntityId value was: $EntityId"
+	        }
+    
+            Write-Host "Request to change owner to entity $EntityId successfully submitted and will propagate over time depending on the size of the folder."
+            Write-Host "Please do not close this PowerShell window; otherwise, the propagation will be cancelled."
+            
+            Set-AzureRmDataLakeStoreItemOwner -Account $Account -Path $Path -Type $EntityType -Id $EntityId -WarningAction SilentlyContinue | Out-Null
+
+            setownerrec -Account $Account -Path $Path -Id $EntityId -EntityType $EntityType -WarningAction SilentlyContinue | Out-Null
+
+            Write-Host "Done."
+        }
+        catch
+        {
+            Write-Error "Set owner operation failed with the following error: $($error[0])"
+        }
+        break
+    } 
+    “SetAclEntry”  {
+        try
+        {
+            if($EntityType -ieq "other" -and !([string]::IsNullOrEmpty($EntityId)))
+	        {
+		        throw "EntityId is not supported when modifying permissions for 'Other' entity types"
+	        }
 	
-    $executingDir = Split-Path -parent $MyInvocation.MyCommand.Definition
-    $executingFile = Split-Path -Leaf $MyInvocation.MyCommand.Definition
+	        if($EntityType -ine "other" -and [string]::IsNullOrEmpty($EntityId))
+	        {
+		        throw "EntityId is required when modifying permissions for 'User' and 'Group' entity types"
+	        }
+	
+	        $ignored = [Guid]::Empty
+	        if($EntityType -ine "other" -and !([Guid]::TryParse($EntityId, [ref]$ignored)))
+	        {
+		        throw "EntityId must be a valid GUID. EntityId value was: $EntityId"
+	        }
     
-    $profilePath = Join-Path $env:TEMP "jobprofilesession.tmp"
-    if(Test-Path $profilePath)
-    {
-        Remove-Item $profilePath -Force -Confirm:$false
+            Write-Host "Request to add entity $EntityId successfully submitted and will propagate over time depending on the size of the folder."
+            Write-Host "Please do not close this PowerShell window; otherwise, the propagation will be cancelled."
+
+            if($EntityType -ieq "other")
+            {
+                giveaccess -Account $Account -Path $Path -EntityType $EntityType -Permissions $Permissions -IsDefault -WarningAction SilentlyContinue | Out-Null
+            }
+            else
+            {
+                giveaccess -Account $Account -Path $Path -Id $EntityId -EntityType $EntityType -Permissions $Permissions -IsDefault -WarningAction SilentlyContinue | Out-Null
+            }
+
+            setacerec -Account $Account -Path $Path -Permissions $Permissions -Id $EntityId -EntityType $EntityType -WarningAction SilentlyContinue | Out-Null
+
+            Write-Host "Done."
+        }
+        catch
+        {
+            Write-Error "Set ACL operation failed with the following error: $($error[0])"
+        } 
     }
-    
-    Save-AzureRMProfile -path $profilePath | Out-Null
-    
-    Write-Host "Request to add entity: $EntityIdToAdd successfully submitted and will propagate over time depending on the size of the folder."
-    Write-Host "Please do not close this PowerShell window; otherwise, the propagation will be cancelled."
-    if($EntityType -ieq "other")
-    {
-        Set-AzureRmDataLakeStoreItemAclEntry -Account $Account -Path $Path -AceType $EntityType -Permissions $Permissions
-        Set-AzureRmDataLakeStoreItemAclEntry -Account $Account -Path $Path -AceType $EntityType -Permissions $Permissions
-    }
-    else
-    {
-        Set-AzureRmDataLakeStoreItemAclEntry -Account $Account -Path $Path -AceType $EntityType -Id $EntityIdToAdd -Permissions $Permissions
-        Set-AzureRmDataLakeStoreItemAclEntry -Account $Account -Path $Path -AceType $EntityType -Id $EntityIdToAdd -Permissions $Permissions -Default
-    }
-    copyacls -Account $Account -Path $Path -Permissions $Permissions -IdToAdd $EntityIdToAdd -entityType $EntityType -loginProfilePath $profilePath | Out-Null
-}
-catch
-{
-    Write-Error "ACL propagation failed with the following error: $($error[0])"
 }
